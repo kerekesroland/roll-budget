8e54ccf7d708f96de0aeb681f7a2ff887b6939e2
kerekesroland <kerekes98roland@gmail.com>
2023-07-10 09:35:44 +0200

    Chore: Added Sidenavitems to the consts

---
+++ constants/SideNavItems.ts
@@ -0,0 +1,26 @@
+export const SIDENAV_ITEMS = [
+  {
+    name: "Dashboard",
+    icon: "/images/DashboardIcon.svg",
+  },
+  {
+    name: "Budget",
+    icon: "/images/BudgetIcon.svg",
+  },
+  {
+    name: "Analytics",
+    icon: "/images/AnalyticsIcon.svg",
+  },
+  {
+    name: "Reminders",
+    icon: "/images/RemindersIcon.svg",
+  },
+  {
+    name: "Invites",
+    icon: "/images/InvitesIcon.svg",
+  },
+  {
+    name: "Logout",
+    icon: "/images/LogoutIcon.svg",
+  },
+];
