d492ece0c118161540e6a4d5a3114f37cdd9a976
kerekesroland <kerekes98roland@gmail.com>
2023-07-10 09:33:19 +0200

    Feat: Added prisma client and nextauth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,63 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import * as argon2 from "argon2";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        //Check if user exists
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        //Check if user password is correct
+
+        const password = await argon2.verify(
+          user.password,
+          credentials.password
+        );
+
+        if (!password) {
+          throw new Error("Invalid credentials");
+        }
+
+        //Check if user is activated
+
+        if (!user.activated) {
+          throw new Error("User is not activated yet!");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/dashboard",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
