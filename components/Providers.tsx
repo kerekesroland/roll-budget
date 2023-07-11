a2977fcbfb68ef7e41203d161329b7c999c3db46
kerekesroland <kerekes98roland@gmail.com>
2023-07-11 15:43:59 +0200

    Feat: Added RecoilRoot to the PRoviders

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { RecoilRoot } from "recoil";
 import ToastProvider from "./ToastProvider";
 import { SessionProvider } from "next-auth/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
-      {children}
+      <RecoilRoot>{children}</RecoilRoot>
       <ToastProvider />
     </SessionProvider>
   );
