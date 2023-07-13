668112510dfa31ce4b885f52cfafc2c59cf33453
kerekesroland <kerekes98roland@gmail.com>
2023-07-13 22:51:40 +0200

    Feat: Created the register and invitation routes for the api

---
+++ app/api/invitation/route.ts
@@ -0,0 +1,58 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { randomUUID } from "crypto";
+const nodemailer = require("nodemailer");
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  if (!body.email === undefined || body.email === null || body.email === "") {
+    return NextResponse.json(
+      {
+        message: "You have to provide an email address!",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const token = await prisma.activateToken.create({
+      data: {
+        token: `${randomUUID()}${randomUUID()}`.replace(/-/g, ""),
+        email: body.email as string,
+      },
+    });
+    const transporter = nodemailer.createTransport({
+      host: "localhost",
+      port: 1025,
+      ignoreTLS: true,
+    });
+
+    const message = {
+      from: "roll@app.com",
+      to: body.email,
+      subject: "Activate account",
+      text: `Please activate your account by clicking on this link: http://localhost:3000/activate-account/${token.token}`,
+    };
+
+    transporter.sendMail(message, (err: any, info: any) => {
+      if (err) {
+        console.error("Error sending email:", err);
+      } else {
+        console.log("Email sent:", info);
+      }
+    });
+    return NextResponse.json({ message: `Invitation sent to ${body.email}` });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Error sending email",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
---
+++ app/api/register/route.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import argon2 from "argon2";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  // console.log(body);
+  if (!body) {
+    return NextResponse.json(
+      {
+        message: "Error",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const userPassword = await argon2.hash(body.password);
+
+  try {
+    const createdUser = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: userPassword,
+        activated: true,
+        activateToken: {
+          connect: {
+            id: body.token.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({ message: `Created user ${createdUser.id}` });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "Error",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
