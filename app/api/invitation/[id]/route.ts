82641a1cdbc6521b87b961f9feecda63215cba1a
kerekesroland <kerekes98roland@gmail.com>
2023-07-14 21:07:32 +0200

    Feat: Added DeleteInvitation logic

---
+++ app/api/invitation/[id]/route.ts
@@ -0,0 +1,58 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const token = await prisma.activateToken.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+
+    if (!token) {
+      return NextResponse.json(
+        {
+          message: "Invitation not found!",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    if (token.userId) {
+      return NextResponse.json(
+        {
+          message: "Cannot delete already accepted invitations!",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await prisma.activateToken.delete({
+      where: {
+        id: params.id,
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      message: `Successfully deleted invitation!`,
+    });
+  } catch (error) {
+    console.error("Error deleting invitation:", error);
+    return NextResponse.json(
+      {
+        message: "Failed to delete invitation.",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
