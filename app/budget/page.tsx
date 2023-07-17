a905fe604493722d0b3e9c0a9148d6be0739b6ac
kerekesroland <kerekes98roland@gmail.com>
2023-07-17 18:47:13 +0200

    Update: Minor changes in the layout

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,13 +1,16 @@
 import BudgetList from "@/components/BudgetList";
+import { getBudgets } from "@/lib/getBudgets";
 import { getCategories } from "@/lib/getCategories";
 import getCurrentUser from "@/lib/getCurrentUser";
 
 const Budget = async () => {
   const user = await getCurrentUser();
   const categories = await getCategories(user?.id as string);
+  const budgets = await getBudgets(user?.id as string);
+
   return (
     <div className="min-h-screen overflow-hidden p-[1.5rem] xs:p-12 md:py-8 lg:py-12 w-full md:w-[calc(100%-300px)]">
-      <BudgetList user={user} categories={categories} />
+      <BudgetList budgets={budgets} user={user} categories={categories} />
     </div>
   );
 };
