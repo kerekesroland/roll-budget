a40499e3c8c0fe8b7eea59008bec0a0535d35cd9
kerekesroland <kerekes98roland@gmail.com>
2023-09-01 18:30:04 +0200

    feat: added utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,49 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+type Months = {
+  [key: number]: string;
+};
+
+type PriceStyle = "decimal" | "currency" | "percent";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function mapDateToMonth(date: Date) {
+  const months: Months = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
+  const readableDate = date.getMonth() + 1;
+  return months[readableDate];
+}
+
+export function formatePrice(
+  price: number,
+  style?: PriceStyle,
+  decimals?: number
+) {
+  const options = {
+    style: style ?? "decimal", // You can also use 'currency', 'percent', etc. depending on your needs
+    minimumFractionDigits: decimals ?? 0, // Specify the number of decimal places
+    maximumFractionDigits: decimals ?? 0, // Specify the number of decimal places
+  };
+
+  return price.toLocaleString("de-DE", options);
 }
