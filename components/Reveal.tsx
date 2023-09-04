08aea9d8145fd8017d0cb3653eca971d2318ff74
kerekesroland <kerekes98roland@gmail.com>
2023-09-04 21:59:21 +0200

    feat: added Reveal component for animating text

---
+++ components/Reveal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+
+const Reveal = ({
+  children,
+  delay,
+}: {
+  children: React.ReactNode;
+  delay: number;
+}) => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const animateReveal = async () => {
+      await controls.start({ width: "0%" });
+    };
+    animateReveal();
+  }, [controls]);
+
+  return (
+    <div className="relative inline-block">
+      <motion.div
+        className="w-full h-full absolute z-20 inset-0 bg-gradient-to-r from-[#4131FF] to-[#8E5BF9]"
+        initial={{ width: "100%" }}
+        animate={controls}
+        transition={{ duration: 1, delay: delay, ease: "easeInOut" }}
+      ></motion.div>
+      <div className="relative z-10">{children}</div>
+    </div>
+  );
+};
+
+export default Reveal;
