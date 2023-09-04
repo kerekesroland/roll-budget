de0733331f5cf1497d795721d69a4bb3bebd11b3
kerekesroland <kerekes98roland@gmail.com>
2023-09-04 21:55:36 +0200

    update: getExceededCategories now has a take argument

--- lib/getExceededCategories.ts
+++ lib/getExceededCategories.ts
@@ -1,10 +1,9 @@
-import { NextResponse } from "next/server";
 import getCurrentUser from "./getCurrentUser";
 import { prisma } from "./prisma";
 import { ICategory } from "@/models/Category";
 import { capitalizeFirstLetter } from "./utils";
 
-export const getExceededCategories = async () => {
+export const getExceededCategories = async (take?: number) => {
   const user = await getCurrentUser();
   try {
     const res = await prisma.category.findMany({
@@ -17,7 +16,7 @@ export const getExceededCategories = async () => {
       orderBy: {
         current: "desc",
       },
-      take: 4,
+      take: take,
     });
     const withCorrectIcon: ICategory[] = res.map((item: ICategory) => {
       return {
