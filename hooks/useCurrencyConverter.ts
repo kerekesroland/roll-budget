77f2199a85e3b6bc2fa2b1a2583c2f37473ae42c
kerekesroland <kerekes98roland@gmail.com>
2023-09-04 21:56:30 +0200

    feat: added useCurrencyConverter hook

---
+++ hooks/useCurrencyConverter.ts
@@ -0,0 +1,39 @@
+"use client";
+
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+
+export type TCurrencies = "USD" | "EUR" | "GBP" | "HUF";
+
+interface IResponseProps {
+  [key: string]: {
+    [property: string]: string;
+  };
+}
+
+const useCurrencyConverter = (
+  fromCurrency: TCurrencies,
+  toCurrency: TCurrencies
+) => {
+  const [value, setValue] = useState<string>("");
+
+  useEffect(() => {
+    const currencyRequest = async () => {
+      try {
+        const res: AxiosResponse<IResponseProps> = await axios.get(
+          `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency.toLowerCase()}.json`
+        );
+        setValue(res.data.usd[`${toCurrency.toLowerCase()}`]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    currencyRequest();
+  }, [fromCurrency, toCurrency]);
+
+  return {
+    value,
+  };
+};
+
+export default useCurrencyConverter;
