24dce880efff44954289bb0604055e43523d7efd
kerekesroland <kerekes98roland@gmail.com>
2024-05-04 13:57:06 +0200

    chore: changed the seeder functions params

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,11 +5,11 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.user.upsert({
     where: {
-      email: "rolca@rolca.com",
+      email: "rolltest@gmail.com",
     },
     update: {},
     create: {
-      email: "rolca@rolca.com",
+      email: "rolltest@gmail.com",
       password:
         "$argon2id$v=19$m=65536,t=3,p=4$PjYhxr2+5QepkVuhYvv2xA$80pLh3cM3vwGuWEPih6i91Ki6Uey0234qoDynGnNe5Q",
       activated: true,
